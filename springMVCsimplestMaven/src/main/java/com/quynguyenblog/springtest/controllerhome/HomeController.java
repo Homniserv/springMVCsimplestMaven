782d90b7b6985cd3a9a151297cc5b39d81cbb055
Peter MOUEZA <mouezapeter@gmail.com>
2018-06-19 12:41:41 +0200

    Resolves #5 no red. Mappings OK(/uli /inuits1 /inuits/inuits1)
    ...so both servlets

--- springMVCsimplestMaven/src/main/java/com/quynguyenblog/springtest/controllerhome/HomeController.java
+++ springMVCsimplestMaven/src/main/java/com/quynguyenblog/springtest/controllerhome/HomeController.java
@@ -1,22 +1,42 @@
 package com.quynguyenblog.springtest.controllerhome;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.quynguyenblog.springtest.controllerinuits.InuitsController;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/uli", method = RequestMethod.GET)
 	public String home() {
+		Logger logger = LogManager.getLogger(HomeController.class);
+		logger.error("in home  /uli");
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/uli1", method = RequestMethod.GET)
+	public String home5() {
+		Logger logger = LogManager.getLogger(HomeController.class);
+		logger.error("in home  /uli1");
+		return "home";
+	}
+
+	@RequestMapping(value = "/springMVCsimplestMaven/uli1", method = RequestMethod.GET)
+	public String home4() {
+		Logger logger = LogManager.getLogger(HomeController.class);
+		logger.error("in home4 /springMVCsimplestMaven/uli1");
+		return "home";
+	}
+
 	@RequestMapping(value = "/uli2", method = RequestMethod.GET)
 	public String home2() {
 		return "home2";
 	}
-	
+
 	@RequestMapping(value = "/springMVCsimplestMaven/", method = RequestMethod.GET)
 	public String home3() {
 		return "home3";
